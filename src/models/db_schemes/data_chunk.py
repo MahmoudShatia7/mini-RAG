282201cf64644f3d3bd9ba52ef21aa1b23fc2117
Mahmoud Shatia <shatiamahmoud33@gmail.com>
2026-06-14 03:19:37 +0300

    link processed chunks to uploaded assets

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -9,20 +9,19 @@ class DataChunk(BaseModel):
     chunk_metadata: dict
     chunk_order: int = Field(..., gt=0)
     chunk_project_id : ObjectId
+    chunk_asset_id: Optional[ObjectId] = None
 
     model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
 
 
     @classmethod
-    def get_indexes(cls) :
-                return [
+    def get_indexes(cls):
+        return [
             {
-                "key" :[
-                    ("chunk_project_id",1)
+                "key": [
+                    ("chunk_project_id", 1)
                 ],
-                "name" : "chunk_project_id_index_1",
-                "unique" : False
+                "name": "chunk_project_id_index_1",
+                "unique": False
             }
         ]
-
- 
