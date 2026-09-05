b23ffe4303b9d874f7abf26fd97b56df0ad3c594
Mahmoud Shatia <shatiamahmoud33@gmail.com>
2026-09-05 19:49:16 +0300

    chore(config): update dependencies and environment settings

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
     settings = get_settings()
 
     postgres_conn= f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
-    app.db_engine = create_async_engine(postgres_conn, echo=True)
+    app.db_engine = create_async_engine(postgres_conn, echo=False)
 
     app.db_client = sessionmaker(app.db_engine, class_=AsyncSession, expire_on_commit=False)
 
